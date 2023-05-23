f2fbe03da04c85921cb6068d2d18e44162642d54
ASKK <102436728+ASKiere@users.noreply.github.com>
2023-05-23 10:33:10 -0400

    added code

---
+++ jost.c
@@ -0,0 +1,26 @@
+//josty oh me!
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void main() 
+{
+    int jost;
+    char charrr;
+    FILE *fptr;
+
+    fptr = fopen("josty oh me!.txt", "a");
+
+    if (fptr == NULL) 
+    {
+        printf("error");
+        exit(1);
+    }
+    while(1)
+    {
+        for (jost=0;jost<19999988;jost++) 
+        {
+            fprintf(fptr, "josty oh me! josty oh me! josty oh me! josty oh me! josty oh me! josty oh me! josty oh me! josty oh me! josty oh me! josty oh me! josty oh me! josty oh me! josty oh me! josty oh me! josty oh me! ", charrr);
+        }
+    }
+}
